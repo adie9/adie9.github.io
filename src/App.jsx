b9455bd5dae49cfece899d8efac2e7a1fa0fd1b3
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-10-06 23:36:45 -0400

    Styled hero section

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="wrapper">
-        <div className="hero flex flex-col justify-center items-center">
-          <h1 className='text-6xl text-center'>Hi, I'm Adrian Guerra</h1>
-          <p className='text-3xl text-center'>A passionate learner turning designs into responsive, accessible web interfaces.
-            I build with React, TailwindCSS, and a love for clean, user-friendly experiences.</p>
+      <div className="wrapper p-4">
+        <div className="hero flex flex-col justify-center items-center gap-4 md:flex-row">
+          <div className='flex flex-col items-center gap-4 md:w-1/2'>
+            <h1 className='text-6xl text-center'>Hi, I'm Adrian Guerra</h1>
+            <p className='text-3xl text-center'>A passionate learner turning designs into responsive, accessible web interfaces.
+              I build with React, TailwindCSS, and a love for clean, user-friendly experiences.</p>
+            <a className='rounded px-3 py-3 bg-primary w-[100px] text-center cursor-pointer' href='#projects'>Projects</a>
+          </div>
+          <div className='md:w-1/2'>
+            <img src='/images/responsive_design.svg' alt='monitor+phone'></img>
+          </div>
         </div>
       </div>
     </>

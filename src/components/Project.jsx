d2269459ad295f54baf7a1f5af90aa3d9821bef1
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-10-07 09:31:37 -0400

    Created Project component and projects.json

---
+++ src/components/Project.jsx
@@ -0,0 +1,19 @@
+
+
+const Project = ({ title, description, image, demo}) => {
+    return (
+        <section className='project'>
+            <a href={demo}>
+                <div className='project-card bg-secondary text-dark w-[300px] rounded hover:-translate-y-2 transition'>
+                    <img className='rounded-t' src={image} alt='project preview'></img>
+                    <div className='p-2'>
+                        <h3 className='italic font-bold'>{title}</h3>
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </a>
+        </section>
+    )
+}
+
+export default Project
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import projects from '../projects.json/'
+import Project from './Project'
 
 const Projects = () => {
     return (
         <section id='projects' className='projects flex flex-col gap-4'>
             <h2 className='text-4xl text-center'>Projects</h2>
-            <a href='https://browser-extensions-manager-ui-main-gules.vercel.app/'>
-                <div className='project-card bg-secondary text-dark w-[300px] rounded hover:-translate-y-2 transition'>
-                    <img className='rounded-t' src='/images/browser_extensions_manager_ui_preview.jpg' alt='project preview'></img>
-                    <div className='p-2'>
-                        <h3 className='italic font-bold'>Browser Extensions Manager UI</h3>
-                        <p>A responsive interface for managing browser extensions, built with React and Sass.</p>
-                    </div>
-                </div>
-            </a>
+            {/* Mapping over projects in projects.json */}
+            { projects.map((project, index) => (
+                <Project key={index} 
+                title={project.title}
+                description={project.description}
+                image={project.image}
+                demo={project.demo} />
+            ))}
+            
         </section>
     )
 }

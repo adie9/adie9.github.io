bda4aba2bbd1da7e680e4be65d3b1d1784df58d9
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-10-06 22:34:42 -0400

    Created images folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <>
       <div className="wrapper">
-          <div className="hero flex flex-col justify-center items-center">
-              <h1 className='text-6xl text-center'>Hi, I'm Adrian Guerra</h1>
-              <p className='text-3xl text-center'>A passionate learner turning designs into responsive, accessible web interfaces.
-                I build with React, TailwindCSS, and a love for clean, user-friendly experiences.</p>
-          </div>
+        <div className="hero flex flex-col justify-center items-center">
+          <h1 className='text-6xl text-center'>Hi, I'm Adrian Guerra</h1>
+          <p className='text-3xl text-center'>A passionate learner turning designs into responsive, accessible web interfaces.
+            I build with React, TailwindCSS, and a love for clean, user-friendly experiences.</p>
+        </div>
       </div>
     </>
   )

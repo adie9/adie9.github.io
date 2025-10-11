c40865e4d77e9b9e4f5c43cae4c1498713e018a8
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-10-11 12:34:25 -0400

    Changed project padding

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,7 @@ const Project = ({ title, description, image, demo}) => {
             <a href={demo}>
                 <div className='project-card bg-secondary text-dark w-[300px] rounded hover:-translate-y-2 transition'>
                     <img className='rounded-t' src={image} alt='project preview'></img>
-                    <div className='p-2'>
+                    <div className='px-2 py-4'>
                         <h3 className='italic font-bold'>{title}</h3>
                         <p>{description}</p>
                     </div>
